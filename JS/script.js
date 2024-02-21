a026dd10149ecee7ca00b823806fecfb7022c9c4
anupriya <anupriya.jakka@gmail.com>
2024-02-20 17:20:29 -0800

    Complete

---
+++ JS/script.js
@@ -0,0 +1,136 @@
+//STEP 1- Camel casing
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let MyLibraryFunction
+
+//STEP 2
+// 23    
+// 'rain' 
+// true 
+// null  
+
+
+//STEP 3
+// let aVariableExp= (3+2)*6
+
+
+//STEP 4
+// //Camel Casing
+// let firstName 
+// let lastName
+// let address
+// let city
+// let state
+// let zipCode 
+// let yourAge
+// let referralSource
+// let mayWeContactYou
+
+// //Hungarian notation
+// let strFirstName 
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let intZipCode 
+// let intYourAge
+// let strReferralSource
+// let blnMayWeContactYou
+
+// //STEP 5
+// let firstName
+// let lastName
+// firstName='Anupriya'
+// lastName='Jakka Suresh'
+
+// let intZipCode=92128, blnMayWeContactYou=false
+
+// let strCity='San Diego'
+// let state='California'
+
+// //STEP 6
+// let aVariable=3+'four';
+// console.log(aVariable)
+
+// //STEP 7
+// let variableOne=false+'sampleString'
+// let variableTwo= 3+true
+// console.log(variableOne)
+// console.log(variableTwo)
+
+// //STEP 8
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+// console.log(someString)
+
+// //STEP 9
+// let variableNullVal=null;
+// console.log(variableNullVal)
+// let variableUndefinedVal;
+// console.log(variableUndefinedVal)
+
+// //STEP 10
+// console.log(typeof("San Diego"));
+// console.log(typeof(123));
+// console.log(typeof(true));
+// console.log(typeof(Math));
+// console.log(typeof(aVariable));
+
+
+// //STEP 11
+// window.alert("Hello "+ "Anupriya"+' '+"Jakka Suresh"+", welcome to the JavaScript class!")
+
+// //STEP 12
+// let myName='Anupriya Jakka Suresh';
+// window.alert("Hello "+ myName +", welcome to the JavaScript class!")
+
+// //STEP 13
+// let myName='Anupriya Jakka Suresh';
+// let course='JavaScript';
+// window.alert("Hello "+ myName +", welcome to the "+ course + " class!")
+
+// //STEP 14
+// let myName='Anupriya Jakka Suresh';
+// let course='JavaScript';
+// window.alert("Hello "+ myName +".\n"+"Welcome to the "+ course + " class!")
+
+// //STEP 15
+// let userName=window.prompt("Enter your name:",'')
+// let course='JavaScript';
+// window.alert("Hello "+ userName +".\n"+"Welcome to the "+ course + " class!")
+
+// //STEP 16
+// let userName=window.prompt("Enter your name:",'')
+// let course=window.prompt("Enter your course:",'')
+// window.alert("Hello "+ userName +".\n"+"Welcome to the "+ course + " class!")
+
+// //STEP 17
+// let x=10;
+// let y=20;
+// console.log(x+y);
+
+// //STEP 18
+// let x=20;
+// x+=20
+// console.log(x);
+
+// //STEP 19
+// let x=20;
+// x*=5
+// console.log(x);
+
+// //STEP 20
+// let x=20%3;
+// x/=1
+// console.log(x);
+
+// //STEP 21
+// let x=1
+// let y=2
+// console.log(x==1 && y==2) 
+
+// //STEP 22
+// let x=1
+// let y=2
+// console.log(x!=1 || y!=2) 
